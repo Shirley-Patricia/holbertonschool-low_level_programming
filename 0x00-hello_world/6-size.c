33a8bc5fe9a58f438781c83349eeb28532bbd6ca
Shirley-Patricia <shircar16@gmail.com>
2021-06-18 04:49:37 +0000

    Sizeof with space after ,

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-Printf("Size of a char: %i byte(s)\n",sizeof(char));
-Printf("Size of an int: %i byte(s)\n",sizeof(int));
-Printf("Size of a long int: %i byte(s)\n",sizeof(long int));
-Printf("Size of a long long int: %i byte(s)\n",sizeof(long long int));
-Printf("Size of a float: %i byte(s)\n",sizeof(float));
+Printf("Size of a char: %i byte(s)\n", sizeof(char));
+Printf("Size of an int: %i byte(s)\n", sizeof(int));
+Printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+Printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+Printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 }
