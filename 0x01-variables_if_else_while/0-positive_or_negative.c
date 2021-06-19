f390ecdb8e4a5e4cd97fd4a317f1484ee8bec8de
Shirley-Patricia <shircar16@gmail.com>
2021-06-19 20:39:36 +0000

    code in other side, haven't vble defined in printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive\n");
+  printf("%d is positive\n", n);
 }
 if (n == 0)
 {
-printf("is zero\n");
+  printf("%d is zero\n", n);
 }
 if (n < 0)
 {
-printf("is negative\n");
+  printf("%d is negative\n", n);
 }
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
 return (0);
 }
