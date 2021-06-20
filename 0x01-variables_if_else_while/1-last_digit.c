6907f0165a8342db4a21da725d06df05da80b97c
Shirley-Patricia <shircar16@gmail.com>
2021-06-20 00:06:34 +0000

    Change If (n per if (lastDigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,14 +13,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 lastDigit = n % 10;
-if (n > 5)
+if (lastDigit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
 }
-if (n == 0)
+if (lastDigit == 0)
 {
 printf("Last digit of %d is %d and is 0", n, lastDigit); }
-if (n < 6 && n != 0)
+if (lastDigit < 6 && lastDigit != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit); }
 return (0);
