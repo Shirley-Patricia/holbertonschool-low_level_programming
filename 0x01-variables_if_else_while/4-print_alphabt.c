c069401364f37f5832f7a9ff6dbc63d44c5bcbef
Shirley-Patricia <shircar16@gmail.com>
2021-06-20 03:23:25 +0000

    alphabet whit if m !=e..

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int m;
+for (m = 'a'; m <= 'z'; m++)
+if (m != 'e' || m != 'q')
+putchar(m);
+putchar('\n');
+return (0);
+}
