dd71cd5fe55868e20da4e7f51cbe23cda402fb28
Shirley-Patricia <shircar16@gmail.com>
2021-06-20 02:08:33 +0000

    int c into brace of the for

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int m;
+for (m = 'a'; m <= 'z'; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
