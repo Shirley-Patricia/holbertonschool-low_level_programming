8a72666791e7c60c63c3fbe0189b41738d0b5811
Shirley-Patricia <shircar16@gmail.com>
2021-06-20 19:15:05 +0000

    Hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a = 48;
+	int m = 97;
+
+	while (a < 58)
+	{
+		putchar (a);
+		a++;
+	}
+	while (m < 103)
+	{
+		putchar (m);
+		m++; }
+	putchar('\n');
+	return (0);
+	}
