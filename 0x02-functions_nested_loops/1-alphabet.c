a9679510df0ef8a7c0badc81608b82201942b143
Shirley-Patricia <shircar16@gmail.com>
2021-06-22 12:59:42 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	int m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+{
+	int m = 0, n;
+	while (m < 10)
+	{
+		n = 'a'
+		while (n <= 'z')
+		{
+		_putchar(n);
+		n++;
+		}
+		_putchar('\n');
+		m++; }
+}
