840e95dc8db33e29feb9aeb40bbac834ee2cb74a
Shirley-Patricia <shircar16@gmail.com>
2021-06-22 22:58:20 +0000

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _abs - give absolute value of a number.
+ * @a: is a integer
+ *
+ * Return: Always 0.
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a *= -1;
+	}
+		return (a);
+}
