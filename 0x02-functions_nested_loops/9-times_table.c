6d6cb0ad193827dc92bffca3852ddccf5c26ce67
Shirley-Patricia <shircar16@gmail.com>
2021-06-24 00:27:28 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,31 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar(i * j + '0');
-			_putchar(',');
-			_putchar(32); }
-		{
-			_putchar(10); }
+			k = i * j;
+			if (k < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' '); }
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		 _putchar('\n');
 	}
 }
 
