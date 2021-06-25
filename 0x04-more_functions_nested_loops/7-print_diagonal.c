544dd3ba3b653d517043f0a8ddd97ae861b9fd14
Shirley-Patricia <shircar16@gmail.com>
2021-06-25 23:01:42 +0000

    Drawn diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Draws diagonal line, n times.
+ * @n: number times that printed the diagonal
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+		_putchar('\n');
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' '); }
+	}
+}
