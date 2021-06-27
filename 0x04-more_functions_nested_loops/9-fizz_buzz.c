c59c6948e2250bd676e032e79fcb7a9b81a8ef0d
Shirley-Patricia <shircar16@gmail.com>
2021-06-27 01:45:13 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,11 @@
 #include "stdio.h"
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int a;
@@ -12,13 +14,13 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz ");
-		else if (a % 5 == 0)
-			printf("Buzz ");
 		else if (a % 3 == 0)
 			printf("Fizz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
 		else
 			printf("%d ", a);
 	}
 	printf("\n");
-	return (0);
+return (0);
 }
