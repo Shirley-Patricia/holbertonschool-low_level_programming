302a26a37b672c92f02b4e28f6b7b19ccb597a76
Shirley-Patricia <shircar16@gmail.com>
2021-06-30 02:19:56 +0000

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print half of a string.
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int n, l, i;
+
+	l = 0;
+	while (*(str + l) != '\0')
+	{
+		l++;
+	}
+	n = ((l - 1) / 2);
+	if (l % 2 == 0)
+	{
+		for (i = n + 1; *(str + i); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = n; *(str + i); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
