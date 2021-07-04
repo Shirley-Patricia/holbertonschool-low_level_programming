0adda457c3a5fc95ed88d1219b68796084622f81
Shirley-Patricia <shircar16@gmail.com>
2021-07-04 19:00:12 +0000

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,25 +11,16 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 0; s1[i] != 0; i++)
 	{}
 	for (j = 0; s2[j] != 0; j++)
 	{}
-	if (i < j || i > j || i == j)
-	{
-		k = i - j;
-	}
-	if (k < 0)
-	{
+	if (i < j)
 		return (-15);
-	}
-	if (k > 0)
-	{
+	if (i > j)
 		return (15);
-	}
-	if (k == 0)
+	if (i == j)
 	{
 		return (0);
 	}
