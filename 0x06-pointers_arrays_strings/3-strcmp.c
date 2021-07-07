9bed736a83e6be8eeede581f5a27998a38c97a73
Shirley-Patricia <shircar16@gmail.com>
2021-07-07 14:54:55 +0000

    correction the complete code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j;
 
-	for (i = 0; s1[i] != 0; i++)
-	{}
-	for (j = 0; s2[j] != 0; j++)
-	{}
-	if (i < j)
-		return (-15);
-	if (i > j)
-		return (15);
-	if (i == j)
+	i = 0;
+	if (s1[i] != s2[i])
 	{
-		return (0);
+		return (s1[i] - s2[i]);
 	}
 	return (0);
 }
