7fe56629c2e59dc6e21530bb68b86a9b81c565d7
Shirley-Patricia <shircar16@gmail.com>
2021-07-08 05:37:02 +0000

    power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - power of a number.
+ * @x: base
+ * @y: power
+ *
+ * Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
