ed7a478ec9a1ae5435584cb5dcee381eecb1054e
Shirley <shircar16@gmail.com>
2021-07-14 15:58:42 -0500

    correction betty

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,13 +13,13 @@ char *_strdup(char *str)
 	int i;
 	int j;
 	char *s;
-	
+
 	if (str == 0)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 	{}
-	s = malloc(((sizeof(char) * i)+1));
+	s = malloc(((sizeof(char) * i) + 1));
 	if (s == 0)
 		return (0);
 	for (j = 0; j <= i; j++)
