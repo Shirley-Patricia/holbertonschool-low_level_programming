f009eaab78f022c45247e12ed030ed1379dddbc6
Shirley <shircar16@gmail.com>
2021-07-15 07:15:00 -0500

    concataned string whit malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,8 @@ char *str_concat(char *s1, char *s2)
 	int j;
 	char *dest;
 
-	if (s1 == 0 && s2 == 0)
-		return (NULL);
+	if (s1 == NULL && s2 == NULL)
+		return (0);
 
 	for (i = 0; s1[i]; i++)
 	{}
