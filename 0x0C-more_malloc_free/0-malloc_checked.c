b53008881cc75d0da5e47d52b03242a17eaa3125
Shirley <shircar16@gmail.com>
2021-07-16 13:18:29 -0500

    allocation memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: bytes in memory
+ *
+ * Return: Always 0 (Success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(sizeof(unsigned int) * b);
+	if (ptr == NULL)
+	{
+		*ptr = 98;
+	}
+	return (ptr);
+	}
