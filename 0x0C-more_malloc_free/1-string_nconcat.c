daecdffa7827990ef07a787557af9d46ca3f40af
Shirley <shircar16@gmail.com>
2021-07-16 19:02:47 -0500

    concataned string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,17 +21,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != 0; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		str = malloc(sizeof(char) * (i + n));
-		if (str == NULL)
-			return (NULL);
 	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+	str = malloc(sizeof(char) * (i + n) + 1);
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; s1[i]; i++)
 	{
 		str[i] = s1[i];
 	}
-	for (j = 0; s2[j] != 0; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 	{
 		if (j < n)
 		{
