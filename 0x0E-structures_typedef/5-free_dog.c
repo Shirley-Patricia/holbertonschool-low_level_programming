5fa7a17269a6e7fbcddeb70415eacb1be20b460a
Shirley <shircar16@gmail.com>
2021-07-20 22:33:59 -0500

    free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+/**
+* free_dog- create a new dog.
+* @d: point
+* *
+* Return: return 0.
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
