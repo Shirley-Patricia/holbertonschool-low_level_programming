5a3fc5d14c4720f45ae053e49495acc977e6bf6d
Shirley <shircar16@gmail.com>
2021-07-21 22:37:14 -0500

    function of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - prints a name
+ * @array: array
+ * @size: size of the array
+ * @action: function to give each element of an array
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
