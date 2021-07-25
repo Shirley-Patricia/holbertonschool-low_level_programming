fe65bc2ebc666dc9a12051047c6c0497cb15e201
Shirley <shircar16@gmail.com>
2021-07-25 00:00:23 -0500

    print any

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,20 @@
 #ifndef _VARIADIC_FUNCTION_H
 #define _VARIADIC_FUNCTION_H
+#include <stdarg.h>
 
-int _putchar(char c);
+typedef struct format
+{
+	char *form;
+	void (*print)(va_list any);	
+} format_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list any);
+void print_int(va_list any);
+void print_float(va_list any);
+void print_string(va_list any);
 
 #endif
