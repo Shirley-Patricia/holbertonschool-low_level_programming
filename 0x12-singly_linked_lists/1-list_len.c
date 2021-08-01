add08687980e33e154b1193bf99a2bf9774feaef
Shirley <shircar16@gmail.com>
2021-08-01 15:43:24 -0500

    return number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,11 +5,21 @@
 /**
  * list_len - prints the elements of the singly linked list
  * @h: name list
- * 
- * Description: singly linked list node structure
+ *
+ * Return: a value
  */
 
 size_t list_len(const list_t *h)
 {
+	const list_t *temp;
+	int count;
 
+	temp = h;
+    count = 0;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return (count);
 }
