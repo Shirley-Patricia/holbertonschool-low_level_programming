f8b3c56f8f586d748eb4145882cc0f1be39b4ef5
Shirley <shircar16@gmail.com>
2021-08-02 16:47:42 -0500

    addittion a new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,15 +1,33 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
- * print_list - prints the elements of the singly linked list
- * @h: name list
- * 
+ * add_node - prints the elements of the singly linked list
+ * @head: name list
+ * @str: string
+ *
  * Description: singly linked list node structure
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+	int lon = 0;
 
+	if (head == NULL || str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	while (str[lon])
+	{
+		lon++;
+	}
+	new_node->len = lon;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
