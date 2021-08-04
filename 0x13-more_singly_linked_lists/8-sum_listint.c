48474b982c93703fd2f0a63ef39708a6af33b3f5
Shirley <shircar16@gmail.com>
2021-08-03 23:02:56 -0500

    sum of all the data of a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sum_listint - sum of all the data of the singly linked list.
+ * @head: head of the list.
+ *
+ * Return: nothing.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum;
+	listint_t *temp;
+
+	temp = head;
+	if (temp == NULL)
+		return (0);
+	sum = 0;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return(sum);
+}
