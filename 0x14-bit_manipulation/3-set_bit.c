6833d57aea9a899256141372e3d6273c3a73b8da
Shirley <shircar16@gmail.com>
2021-08-07 20:35:17 -0500

    set the value of a bit to 1 in a given position

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
 /**
  * set_bit - pritn the binary representation of a number.
  * @n: integer number
- * @index: index of a given binary number 
+ * @index: index of a given binary number
  *
  * Return: nothing
  */
@@ -13,5 +13,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	*n |= (1 << index);
 	return (1);
 	if (index > 8 * (sizeof(n)))
-        return (-1);
+		return (-1);
 }
