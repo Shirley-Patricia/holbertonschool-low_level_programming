0dca8918cf1e25edf8e5f160036ad99ff6b3b4a9
Shirley <shircar16@gmail.com>
2021-08-08 15:38:30 -0500

    bits need to flip one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - return bits that need to flip to get from one number to another.
+ * @n: integer number
+ * @m: integer number
+ *
+ * Return: nothing
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int y;
+	int count = 0;
+
+	y = n ^ m;
+	while (y > 0)
+	{
+		count++;
+		y &= (y - 1);
+	}
+	return (count);
+}
