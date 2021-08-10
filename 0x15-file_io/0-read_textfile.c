ea48bbb07018a82237317f2dda94b84c8adb2287
Shirley <shircar16@gmail.com>
2021-08-09 21:19:12 -0500

    reads a text file and prints in std output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @letters: letters number
+ *
+ * Return: a value
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int file_rd;
+	int file_wr;
+	char *buf;
+	
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc((sizeof(char) * letters));
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	file_rd = read(fd, buf, letters);
+	if (file_rd == -1)
+		return (0);
+
+	file_wr = write(STDOUT_FILENO, buf, file_rd);
+	if (file_wr == -1)
+		return (0);
+	
+	close(fd);
+	free(buf);
+	return (file_rd);
+}
