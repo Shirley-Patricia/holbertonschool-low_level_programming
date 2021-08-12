18e206af3d9a6029bd3a7d7556b945151d491fc5
Shirley <shircar16@gmail.com>
2021-08-11 22:49:19 -0500

    fix betty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 
 /**
- * pop_listint - delete the head node of the singly linked list.
+ * get_nodeint_at_index - returns the nth node of a linked list.
  * @head: head of the list.
+ * @index: index of the node
  *
  * Return: nothing.
  */
@@ -13,9 +14,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 	listint_t *tmp;
-	
+
 	tmp = head;
-			
+
 	while (tmp != NULL)
 	{
 		if (count == index)
