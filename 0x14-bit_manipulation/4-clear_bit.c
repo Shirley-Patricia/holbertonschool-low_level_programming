86407de8daeb6a06b93108e5b691238a45f0d251
Shirley <shircar16@gmail.com>
2021-08-11 22:13:26 -0500

    sets the value of a bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,8 +10,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > 8 * (sizeof(n)))
+	{
+		return (-1);
+	}
 	*n &= ~(1 << index);
 	return (1);
-	if (index > 8 * (sizeof(n)))
-		return (-1);
 }
