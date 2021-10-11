a600df75ecdc8859a8a7b60cbfcd6bd5ea6bf454
Shirley <shircar16@gmail.com>
2021-10-11 00:04:46 -0500

    h

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: key you are looking for
+ *
+ * Return: node.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	printf(" ");
+	(void)ht;
+}
