3021be0bfbd8e200743368585b83eb3151f69271
Shirley <shircar16@gmail.com>
2021-10-10 21:54:06 -0500

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,10 +14,8 @@ hash_node_t *ht_pair(const char *key, const char *value)
 	hash_node_t *entry;
 
 	/*allocate the entry*/
-	entry = malloc(sizeof(entry) * 1);
-	entry->key = malloc(strlen(key) + 1);
-	entry->value = malloc(strlen(value) + 1);
-
+	entry = malloc(sizeof(hash_node_t));
+	
 	/*copy key and value in a place*/
 	strcpy(entry->key, key);
 	strcpy(entry->value, value);
@@ -36,9 +34,14 @@ hash_node_t *ht_pair(const char *key, const char *value)
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int index;
+	unsigned long int index;
 	hash_node_t *entry;
 	hash_node_t *prev;
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL || strlen(key) == 0)
+		return (0);
 	
 	index = key_index((unsigned char *)key, ht->size);
 	entry = ht->array[index];
@@ -46,7 +49,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (entry == NULL)
 	{
 		ht->array[index] = ht_pair(key, value);
-		return (0);
+		return (1);
 	}
 
 	while (entry != NULL)
