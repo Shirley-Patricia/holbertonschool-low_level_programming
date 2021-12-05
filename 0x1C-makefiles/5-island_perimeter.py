cb91751f23b3dfbe7c6f1333bf212e5452200569
Shirley <shircar16@gmail.com>
2021-12-05 17:52:10 -0500

    perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """island perimeter"""
 
+
 def island_perimeter(grid):
     """function return the perimeter of the island"""
     perimeter = 0
     for row in range(len(grid)):
         for j in range(len(grid[row])):
             if (grid[row][j] == 1):
-                if (grid[row][j - 1] == 0):
+                if grid[row][j - 1] == 0:
                     perimeter += 1
-                if (grid[row][j + 1] == 0):
+                if grid[row][j + 1] == 0:
                     perimeter += 1
-                if (grid[row - 1][j] == 0):
-                    perimeter +=1
-                if (grid[row + 1][j] == 0):
-                    perimeter +=1
+                if grid[row - 1][j] == 0:
+                    perimeter += 1
+                if grid[row + 1][j] == 0:
+                    perimeter += 1
     return perimeter
